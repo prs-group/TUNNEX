7216e215c59267c3b75eadb122204ce5c5dce2e0
Henrik Quanz <fhquanz@web.de>
2018-06-14 11:18:27 +0200

    Added license information

--- cmake/CPackVariables_Tunnex.cmake
+++ cmake/CPackVariables_Tunnex.cmake
@@ -1,4 +1,20 @@
 # vim:tabstop=4:shiftwidth=4
+# This file is part of TUNNEX.
+
+# TUNNEX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# laster any later version.
+
+# TUNNEX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with TUNNEX.  If not, see <http://www.gnu.org/licenses/>.
+# TUNNEX 1.0.0 
+# Copyright (c) Henrik Quanz
 INCLUDE(InstallRequiredSystemLibraries)
 INCLUDE(GetGitRevisionDescription)
 
